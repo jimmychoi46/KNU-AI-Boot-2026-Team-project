78dbddfa549322d0520c7795da770e7e126e1064
jimmychoi46 <jimmychoi46@gmail.com>
2026-07-09 11:37:37 +0900

    Delete legacy directory

--- legacy/config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-NAVER_CLIENT_ID = os.getenv("NAVER_CLIENT_ID")
-NAVER_CLIENT_SECRET = os.getenv("NAVER_CLIENT_SECRET")
-SMTP_SENDER = os.getenv("SMTP_SENDER")
-SMTP_PASSWORD = os.getenv("GOOGLE_APP_PASSWORD")
